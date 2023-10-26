fafc1ae9ad12834f1a8cce74a8c4cc491b65584c
Jorge Gonzalez <infjgonzalez@elorrieta-errekamari.com>
2023-10-26 08:05:50 +0200

    FINAL commit

--- src/app/pais/pages/por-subregion/por-subregion.component.ts
+++ src/app/pais/pages/por-subregion/por-subregion.component.ts
@@ -8,13 +8,53 @@ import { Country } from '../../interfaces/pais.interfaces';
   styleUrls: ['./por-subregion.component.css']
 })
 export class PorSubregionComponent {
-  subregiones: string[] = ['Eastern Europe', 'Northern Europe', 'Southern Europe', 'Western Europe'];
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  subregiones: string[] = [];
+  subregionesEuropa: string[] = ['Eastern Europe', 'Northern Europe', 'Southern Europe', 'Western Europe'];
+  subregionesAfrica: string[] = ['Northern Africa', 'Eastern Africa', 'Middle Africa', 'Southern Africa', 'Western Africa'];
+  subregionesAmerica: string[] = ['North America', 'South America', 'Central America', 'Caribbean'];
+  subregionesAsia: string[] = ['Central Asia', 'Eastern Asia', 'South-eastern Asia', 'Southern Asia', 'Western Asia'];
+  subregionesOceania: string[] = ['Australia and New Zealand', 'Melanesia ', 'Micronesia', 'Polynesia'];
+  regionActiva: string = '';
+
   subregionActiva: string = '';
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
 
   constructor(private paisService: PaisService) { }
+
+  activarRegion(region: string) {
+    if (region == this.regionActiva) {
+      return;
+    }
+
+    this.regionActiva = region;
+
+    switch (this.regionActiva) {
+      case 'africa':
+        this.subregiones = this.subregionesAfrica;
+        this.activarSubregion(this.subregiones[0]);
+        break;
+      case 'europe':
+        this.subregiones = this.subregionesEuropa;
+        this.activarSubregion(this.subregiones[0]);
+        break;
+      case 'americas':
+        this.subregiones = this.subregionesAmerica;
+        this.activarSubregion(this.subregiones[0]);
+        break;
+      case 'asia':
+        this.subregiones = this.subregionesAsia;
+        this.activarSubregion(this.subregiones[0]);
+        break;
+      case 'oceania':
+        this.subregiones = this.subregionesOceania;
+        this.activarSubregion(this.subregiones[0]);
+        break;
+    }
+  }
 
   activarSubregion(subregion: string) {
     if (subregion == this.subregionActiva) { return; }
